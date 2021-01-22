97c3e28e66e574129624b9423452f11242e262f4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-01-22 08:49:16 +0530

    creating components

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,25 @@
+import { ApolloClient, fromPromise, gql, InMemoryCache } from '@apollo/client';
+import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Box, Flex, Heading } from '@chakra-ui/react';
 
-export default function Home() {
+export default function Home(results) {
+  const initialState = results;
+  const [characters, setCharacters] = useState(initialState.characters)
+
   return (
-    <div className={styles.container}>
+    <Flex direction="column" justify="center" align="center">
       <Head>
-        <title>Create Next App</title>
+        <title>NextJS Apollo Tutorial</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Box mb={4} flexDir="column" align="center" justify="center" py={8}>
+        <Heading as="h1" size="2xl" mb={8}>
+          Rick and Morty
+        </Heading>
+      </Box>
 
       <footer className={styles.footer}>
         <a
@@ -60,6 +31,50 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
-    </div>
+    </Flex>
   )
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache()
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      {
+        characters(page: 1) {
+          info {
+            count
+            pages
+          }
+          results {
+            name
+            id
+            location {
+              id
+              name
+            }
+            origin {
+              id
+              name
+            }
+            episode {
+              id
+              episode
+              air_date
+            }
+            image
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      characters: data.characters.results,
+    }
+  }
 }
